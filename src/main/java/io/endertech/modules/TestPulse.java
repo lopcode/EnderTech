75307ce4b7390baabc9930d12dabd2771d31dcac
Arkan Emberwalker <arkan@emberwalker.cc>
2014-07-01 14:32:17 +0100

    Upgrade Pulsar to 0.1.0.
    Removes IPulse, uses @Handler instead. This also enables the new config format with module descriptions.

--- src/main/java/io/endertech/modules/TestPulse.java
+++ src/main/java/io/endertech/modules/TestPulse.java
@@ -3,26 +3,26 @@ package io.endertech.modules;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import io.drakon.pulsar.pulse.IPulse;
+import io.drakon.pulsar.pulse.Handler;
 import io.drakon.pulsar.pulse.Pulse;
 import io.endertech.helper.LogHelper;
 
-@Pulse(id = "TestPulse")
-public class TestPulse implements IPulse
+@Pulse(id = "TestPulse", description = "This one's for testing. It goes up to 11.")
+public class TestPulse
 {
-    @Override
+    @Handler
     public void preInit(FMLPreInitializationEvent fmlPreInitializationEvent)
     {
         LogHelper.info("Test pulse preInit");
     }
 
-    @Override
+    @Handler
     public void init(FMLInitializationEvent fmlInitializationEvent)
     {
         LogHelper.info("Test pulse init");
     }
 
-    @Override
+    @Handler
     public void postInit(FMLPostInitializationEvent fmlPostInitializationEvent)
     {
         LogHelper.info("Test pulse postInit");
